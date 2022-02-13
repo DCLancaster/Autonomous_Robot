79a952529736d95a0e6ffc327f921bca431072be
DCLancaster <d.lancaster1@lancaster.ac.uk>
2022-02-13 17:30:16 +0000

    pushed by script

---
+++ commit.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+if [ -d "/home/dc/catkin_ws/src/407/Autonomous_Robot" ]
+then
+  cd /home/dc/catkin_ws/src/407/Autonomous_Robot || exit
+  git add --all
+  # shellcheck disable=SC2035
+  git add *
+  git commit -m "pushed by script"
+  git push origin main
+else
+  echo false
+fi
--- expect_test.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-echo input:
-read input_var
-echo output:
-echo "$input_var"
-echo
